7f7dcb19afae1b9ed71c5191cc6ccf474ddb6d8c
Jordan Jefferson <jjeffe10@gmail.com>
2024-08-03 11:02:51 -0400

    Added syntax highlighting to code in the answers markdown.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import { ErrorResponse, ResponseData } from "./helpers/fetch";
 import FlashCard from "./components/FlashCard";
 
--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
 import { InterviewQuestion } from "../App";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function FlashCard({ question, answer }: InterviewQuestion) {
   const [flipped, setIsFlipped] = useState(false);
   return (
     <div onClick={() => setIsFlipped((prev) => !prev)}>
       {!flipped && <p>{question}</p>}
-      {flipped && <p>{answer}</p>}
+      {flipped && (
+        <ReactMarkdown
+          children={answer}
+          components={{
+            code({ node, className, children, style, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !node?.properties.inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, "")}
+                  language={match[1]}
+                  PreTag="div"
+                  style={atomDark}
+                  // {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        />
+      )}
     </div>
   );
 }
